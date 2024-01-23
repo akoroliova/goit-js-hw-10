d7f080bbcc5a2461781924424d3e831617f49c7f
akoroliova <a.koroliova@gmail.com>
2024-01-23 18:17:22 +0000

    Deploying to gh-pages from @ akoroliova/goit-js-hw-10@a740c268ffc65fcd0da2b18c7fd63276ba2f19e2 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/styles-acf1ecb5.js";import{f as o}from"./assets/vendor-4daf66c6.js";const t={enableTime:!0,dateFormat:"Y-m-d H:i",time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0]),console.log(Date(Date.now)),(e[0]<Date.now||e[0]===null)&&window.alert("Please choose a date in the future")}};o("#datetime-picker",{options:t});
+import"./assets/styles-acf1ecb5.js";import{f as t}from"./assets/vendor-77e16229.js";const o=Date.now(),n={enableTime:!0,dateFormat:"Y-m-d H:i",time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0]),(e[0]<o||e[0]===null)&&window.alert("Please choose a date in the future")}};t("#datetime-picker",{options:n});
 //# sourceMappingURL=commonHelpers.js.map
